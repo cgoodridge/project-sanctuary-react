0c8b54c6c8246967e1e03eddf4719b9cf7cf478b
Che' Goodridge <>
2022-03-16 21:59:24 -0400

    Added modal dialog to map view

--- src/pages/notFoundPage/NotFound.tsx
+++ src/pages/notFoundPage/NotFound.tsx
@@ -1,15 +1,14 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './notFound.css';
-// import * as LottiePlayer from "@lottiefiles/lottie-player";
-import { Player } from '@lottiefiles/react-lottie-player';
-// import Box from ''
 
 const NotFound = () => {
     return (
         <div className='notFoundImage'>
-            <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_4qhciwpm.json" background="transparent" speed="1" style={{ width: '100%', height: '100%' }} loop autoplay></lottie-player>
-
+            <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_03wqck43.json" background="transparent" speed="1" style={{ width: '600px', height: '500px' }} loop autoplay></lottie-player>
+            <h3>Whoops, looks like you took a wrong turn</h3>
+            <h4>Let's get you back home</h4>
+            <Button size="small" variant="contained" color="primary" component={Link} to="/">Take me home</Button>
         </div>
     )
 }
