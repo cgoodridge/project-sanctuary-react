cf2fc0ed7df04eddd71ab30704daca10bc9435bf
cgoodridge <cag45@live.com>
2022-03-23 16:50:32 -0400

    WIP: started implementing search feature

--- src/components/animalListComponent/AnimalListComponent.tsx
+++ src/components/animalListComponent/AnimalListComponent.tsx
@@ -18,12 +18,14 @@ import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
 
+
+
 const AnimalListComponent = () => {
 
     const [animals, setAnimals] = useState<any[]>([]);
     const _isMounted = useRef(true);
 
-    const [loading, setLoading] = useState(true);
+    const [searchQuery, setSearchQuery] = useState('');
 
     const checkWhiteSpace = (name: string) => {
         return name.indexOf(' ') >= 0;
@@ -49,20 +51,26 @@ const AnimalListComponent = () => {
     return (
         <>
             <Box sx={{ margin: '16px 0' }}>
-                <Input
-                    id="input-with-icon-adornment"
-                    placeholder='Search'
-                    startAdornment={
-                        <InputAdornment position="start">
-                            <SearchIcon />
-                        </InputAdornment>
-                    }
-                />
+                <TextField
+                    id="standard-basic"
+                    fullWidth
+                    label="Search"
+                    color="primary"
+                    variant="standard"
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="end">
+                                <SearchIcon />
+                            </InputAdornment>
+                        ),
+                    }} />
             </Box>
 
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }} sx={{ marginTop: "64px", marginBottom: "32px" }}>
 
-                {animals.length <= 0 ? <Box sx={{ display: 'flex', margin: '150px auto' }}> <CircularProgress /> </Box> : animals.map((animal, key) => (
+                {searchQuery === '' ? animals.length <= 0 ? <Box sx={{ display: 'flex', margin: '150px auto' }}> <CircularProgress /> </Box> : animals.map((animal, key) => (
                     <Grid item xs={2} sm={4} md={4} key={key}>
                         <Card sx={{ maxWidth: 300, minWidth: 300 }}>
                             <CardMedia
@@ -84,7 +92,36 @@ const AnimalListComponent = () => {
                             </CardActions>
                         </Card>
                     </Grid>
-                ))}
+                ))
+
+                    :
+
+                    animals.length <= 0 ? <Box sx={{ display: 'flex', margin: '150px auto' }}> <CircularProgress /> </Box> : animals.filter(animal => animal.commonName.toLowerCase().includes(searchQuery.toLowerCase())).map((animal, key) => (
+                        <Grid item xs={2} sm={4} md={4} key={key}>
+                            <Card sx={{ maxWidth: 300, minWidth: 300 }}>
+                                <CardMedia
+                                    component="img"
+                                    height="300"
+                                    image={animal.data.imgURL}
+                                    alt={animal.data.commonName}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {animal.data.commonName}
+                                    </Typography>
+                                    <Typography variant="body2" noWrap color="text.secondary">
+                                        {animal.data.description}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Link key={animal.id} className="learnMore" to={checkWhiteSpace(animal.data.commonName) ? `/animals/${animal.data.commonName.replace(/ /g, "_")}` : `/animals/${animal.data.commonName}`}><Button size="small" >Learn More </Button></Link>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))
+
+
+                }
             </Grid>
             <AddAnimal />
 
