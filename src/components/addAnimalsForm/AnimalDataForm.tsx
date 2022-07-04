b9a8e2eb868f9b37f0ba4f7b21583b5ee22665a2
cgoodridge <cag45@live.com>
2022-07-04 16:39:33 -0400

    Started refactoring process for animal form

--- src/components/addAnimalsForm/AnimalDataForm.tsx
+++ src/components/addAnimalsForm/AnimalDataForm.tsx
@@ -4,20 +4,31 @@ import Box from '@mui/material/Box';
 import { useDispatch } from 'react-redux';
 import { saveData } from '../../slices/formDataSlice';
 
-const AnimalDataForm = ({ saveAnimalData }:any) => {
+const AnimalDataForm = ({ saveAnimalData }: any) => {
 
     const dispatch = useDispatch();
 
+    /// Form values
     const [kingdom, setKingdom] = useState('');
     const [phylum, setPhylum] = useState('');
     const [kingdomClass, setKingdomClass] = useState('');
     const [order, setOrder] = useState('');
     const [family, setFamily] = useState('');
+    const [diet, setDiet] = useState('');
     const [genus, setGenus] = useState('');
     const [species, setSpecies] = useState('');
+    const [lifespan, setLifespan] = useState('');
+    const [lifestyle, setLifestyle] = useState('');
+    const [nameOfYoung, setNameOfYoung] = useState('');
     const [commonName, setCommonName] = useState('');
+    const [groupBehaviour, setGroupBehaviour] = useState('');
+    const [source, setSource] = useState('');
+    const [imageSource, setImageSource] = useState('');
+    const [redListStatus, setRedListStatus] = useState('');
     const [description, setDescription] = useState('');
-    
+    const [locationName, setLocationName] = useState('');
+    /// End form values
+
 
     if (saveAnimalData) {
         dispatch(saveData({
@@ -33,26 +44,28 @@ const AnimalDataForm = ({ saveAnimalData }:any) => {
     }
 
     return (
-        <form>
+        <form id="animalInfoForm">
             <Box sx={{
                 '& > :not(style)': { m: 1, width: '28ch' },
             }}>
                 <TextField
+                    required
                     autoFocus
                     margin="dense"
                     id="kingdom"
                     label="Kingdom"
                     value={kingdom}
-                    onChange={e => setKingdom(e.target.value)}
+                    onChange={(e: any) => setKingdom(e.target.value)}
                     type="text"
                     variant="standard"
                 />
                 <TextField
+                    required
                     autoFocus
                     margin="dense"
                     id="phylum"
                     value={phylum}
-                    onChange={e => setPhylum(e.target.value)}
+                    onChange={(e: any) => setPhylum(e.target.value)}
                     label="Phylum"
                     type="text"
                     variant="standard"
@@ -62,23 +75,25 @@ const AnimalDataForm = ({ saveAnimalData }:any) => {
                 '& > :not(style)': { m: 1, width: '28ch' },
             }}>
                 <TextField
+                    required
                     autoFocus
                     margin="dense"
                     id="kingdomClass"
                     label="Class"
                     value={kingdomClass}
-                    onChange={e => setKingdomClass(e.target.value)}
+                    onChange={(e: any) => setKingdomClass(e.target.value)}
                     type="text"
                     fullWidth
                     variant="standard"
                 />
                 <TextField
+                    required
                     autoFocus
                     margin="dense"
                     id="order"
                     label="Order"
                     value={order}
-                    onChange={e => setOrder(e.target.value)}
+                    onChange={(e: any) => setOrder(e.target.value)}
                     type="text"
                     fullWidth
                     variant="standard"
@@ -89,23 +104,25 @@ const AnimalDataForm = ({ saveAnimalData }:any) => {
                 '& > :not(style)': { m: 1, width: '28ch' },
             }}>
                 <TextField
+                    required
                     autoFocus
                     margin="dense"
                     id="family"
                     label="Family"
                     value={family}
-                    onChange={e => setFamily(e.target.value)}
+                    onChange={(e: any) => setFamily(e.target.value)}
                     type="text"
                     fullWidth
                     variant="standard"
                 />
                 <TextField
+                    required
                     autoFocus
                     margin="dense"
                     id="genus"
                     label="Genus"
                     value={genus}
-                    onChange={e => setGenus(e.target.value)}
+                    onChange={(e: any) => setGenus(e.target.value)}
                     type="text"
                     fullWidth
                     variant="standard"
@@ -116,23 +133,111 @@ const AnimalDataForm = ({ saveAnimalData }:any) => {
                 '& > :not(style)': { m: 1, width: '28ch' },
             }}>
                 <TextField
+                    required
                     autoFocus
                     margin="dense"
                     id="species"
                     label="Species"
                     value={species}
-                    onChange={e => setSpecies(e.target.value)}
+                    onChange={(e: any) => setSpecies(e.target.value)}
                     type="text"
                     fullWidth
                     variant="standard"
                 />
                 <TextField
+                    required
                     autoFocus
                     margin="dense"
                     id="commonName"
                     label="Common Name"
                     value={commonName}
-                    onChange={e => setCommonName(e.target.value)}
+                    onChange={(e: any) => setCommonName(e.target.value)}
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+            </Box>
+
+            <Box sx={{
+                '& > :not(style)': { m: 1, width: '28ch' },
+            }}>
+                <TextField
+                    required
+                    autoFocus
+                    margin="dense"
+                    id="diet"
+                    label="Diet"
+                    value={diet}
+                    onChange={(e: any) => setDiet(e.target.value)}
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+                <TextField
+                    required
+                    autoFocus
+                    margin="dense"
+                    id="lifestyle"
+                    label="Lifestyle"
+                    value={lifestyle}
+                    onChange={(e: any) => setLifestyle(e.target.value)}
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+            </Box>
+
+            <Box sx={{
+                '& > :not(style)': { m: 1, width: '28ch' },
+            }}>
+                <TextField
+                    required
+                    autoFocus
+                    margin="dense"
+                    id="lifespan"
+                    label="Lifespan"
+                    value={lifespan}
+                    onChange={(e: any) => setLifespan(e.target.value)}
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+                <TextField
+                    required
+                    autoFocus
+                    margin="dense"
+                    id="nameOfYoung"
+                    label="Name of Young"
+                    value={nameOfYoung}
+                    onChange={(e: any) => setNameOfYoung(e.target.value)}
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+            </Box>
+            <Box sx={{
+                '& > :not(style)': { m: 1, width: '28ch' },
+            }}>
+                <TextField
+                    required
+                    autoFocus
+                    margin="dense"
+                    id="redListStatus"
+                    label="Red List Status"
+                    value={redListStatus}
+                    onChange={(e: any) => setRedListStatus(e.target.value)}
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+                <TextField
+                    required
+                    autoFocus
+                    margin="dense"
+                    id="groupBehaviour"
+                    label="Group Behaviour"
+                    value={groupBehaviour}
+                    onChange={(e: any) => setGroupBehaviour(e.target.value)}
                     type="text"
                     fullWidth
                     variant="standard"
@@ -148,10 +253,42 @@ const AnimalDataForm = ({ saveAnimalData }:any) => {
                     id="description"
                     label="Description"
                     value={description}
-                    onChange={e => setDescription(e.target.value)}
+                    onChange={(e: any) => setDescription(e.target.value)}
                     type="text"
                     fullWidth
                     multiline
+                    variant="standard"
+                />
+            </Box>
+            <Box sx={{
+                '& > :not(style)': { m: 1, width: '60ch' },
+            }}>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="source"
+                    label="Information Source"
+                    required
+                    value={source}
+                    onChange={(e: any) => setSource(e.target.value)}
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+            </Box>
+            <Box sx={{
+                '& > :not(style)': { m: 1, width: '60ch' },
+            }}>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="imgSrc"
+                    label="Image Source"
+                    required
+                    value={imageSource}
+                    onChange={(e: any) => setImageSource(e.target.value)}
+                    type="text"
+                    fullWidth
                     variant="standard"
                 />
             </Box>
